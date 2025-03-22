d583a36c214332b7eee77eec3b8384e7a2bdca55
Viktor Ilie <viktor.ilie@outlook.com>
2025-03-22 02:38:48 +0100

    update 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <div className="d-flex">
+      <div className="d-flex ">
         <Sidebar />
-        <div className="pt-4 w-100">
+        <div className="pt-1">
           <MainCard />
           <Skills />
           <Portfolio />
